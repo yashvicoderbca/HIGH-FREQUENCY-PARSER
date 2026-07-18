52faccccc5b3c36d3bce0b68a98a852ecca480de
yashvicoderbca <yashvig38@gmail.com>
2026-07-18 22:49:39 +0530

    Update read.cpp

--- read.cpp
+++ read.cpp
@@ -7,9 +7,9 @@
 *TECHNOLOGY: C++,FILE I/O, RESOURSE ALLOCATION, GUARD CLAUSE LOGIC
 *======================================================================================
 */
-#include <iostream>
-#include<fstream>
-#include<string>
+#include <iostream>//required for standard input/output stream operations(eg cout,end)
+#include<fstream> //required for handling file input/output stream pipelines (eg, ifstream, ofstream)
+#include<string>// required for utilization the core c++ string data types and manipulation mechanics
 
 using namespace std;
 //========================================================================================
@@ -36,5 +36,39 @@ class highfrequencyparser{
         // step 4:safely close the pipeline connection to prevent memory leaks
         file.close();
         return true;
+    }
+    /** 
+    * @brief PART 2 NEW: reads the file line by line and tokenizes each row using commas.
+    * @param filename path of the target CSV file
+    */
+    void parse_and_tokenize(const string& filename){
+        // step 1: open stream in read mode to parse data rows
+        ifstream file(filename);
+        if(!file.is_open()){
+            cout<<"[ERROR][PART 2]: FAILED TO OPEN THE FILE FOR TOKENIZATION ENGINE!"<<endl;
+            return;
+        }
+        cout<<"\n[ STARTING PART 2: ROW EXTRACTION AND TOKENIZATION PIPELINE]"<<endl;
+        cout<<"====================================================================";
+        string current_row;
+        int row_counter = 0;
+        //step 2: continuous execution loop to extract rows sequuentially till EOF(END OF FILE)
+        while(getline(file,current_row)){
+            row_counter++;
+            cout<<"\n[ROW #]"<<row_counter<<"RAW TEXT]:"<<current_row<<endl;
+            //step 3: wrap the raw string inside a stream object to slice segments logically
+            stringstream data_stream(current_row);
+            string individual_token;
+            cout<<"-> tokens extracted:";
+            //step 4: internal splitting loop parsing individual data element separated by comma
+            while(getline(data_stream,individual_token,',')){
+                cout<<"["<<individual_token<<"]";
+            }
+            cout<<endl;
+        }
+       cout<<"-------------------------------------------------------------------"<<endl;
+       cout<<"[SUCCESS][PART 2]:total"<<row_counter<<"rows processed and parsed securely.\n"<<endl;
+       // step 5: safely release system hardware descriptors
+       file.close();
     }
 };
