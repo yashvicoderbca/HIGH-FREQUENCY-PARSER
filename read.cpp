4f1d18ab473f5338d3564a54262e9dacb1e602f2
yashvicoderbca <yashvig38@gmail.com>
2026-07-24 22:20:38 +0530

    Update read.cpp

--- read.cpp
+++ read.cpp
@@ -1,21 +1,22 @@
 /** 
 *=======================================================================================
-* HIGH-FREQUENCY DATA PARSER (PROJECT 2 PART 1, PART 2, PART 3, PART 4, PART 5, PART 6)
+* HIGH-FREQUENCY DATA PARSER (PROJECT 2 PART 1, PART 2, PART 3, PART 4, PART 5, PART 6, PART 7)
 *=======================================================================================
 *FILE: read.cpp
-*ROLE: CORE ENGINE, MEMORY ALLOCATION, PARSING, QUERIES AND DIAGNOSTICS
-*TECHNOLOGY: C++,FILE I/O, STRING STREAMS, EXCEPTION HANDLING (TRY-CATCH)
+*ROLE: DYNAMIC MEMORY VECTOR ENGINE, EXCEPTION SAFETY & DISK SYNC
+*TECHNOLOGY: C++, STL VECTOR, FILE I/O, STRING STREAMS, EXCEPTIONS
 *======================================================================================
 */
 #include <iostream>
 #include<fstream>
 #include<string>
 #include<sstream>
+#include <vector>// PART 7: REPLACES FIXED ARRAY WITH DYNAMIC MEMORY ALLOCATION 
 #include <stdexcept>// PART 5 NEW: REQUIRED FOR EXCEPTION HANDLING TYPES
 
+
 using namespace std;
-// MAXUMUM CAPACITY LIMIT DEFINED FOR RAM DATABASE BUFFER
-const int MAX_CAPACITY = 100;
+
 
 //=========================================================================================
 //PART 3 NEW: CUSTOM DATA STRUCTURE FOR OBJECT MAPPING 
@@ -29,8 +30,9 @@ struct Employee{
 };
 class highfrequencyparser {
     public:
-    Employee parsed_database[MAX_CAPACITY];
-    int active_record_count=0;// tracks the number of loaded objects in ram
+    //PART 7 UPGRADE: DYNAMIC MEMORY ALLOCATION USING STL VECTOR(NO FIXED MAX_CAPACITY LIMIT)
+    vector<Employee> parsed_database;
+    
 
     //========================================================================================
 //PART 1: HIGH-PERFORMANCE FILE LOADING ENGINE
@@ -75,28 +77,21 @@ class highfrequencyparser {
          //sequential row extraction loop
          while(getline(file,current_row)){
             row_counter++;
-            // PART 5 NEW: CHECKS ATTAY CAPACITY LIMIT BEFORE PARSING TO PREVENT BUFFER OVERFLOW
-            if(active_record_count>=MAX_CAPACITY){
-                cout<<"[WARNING][PART 5]:database capacity limit reached skipping remaining rows"<<endl;
-                break;
-            }
             stringstream data_stream(current_row);
             string raw_id,raw_name,raw_salary,raw_dept;
             //PART 3 NEW:POSITIONAL DATA EXTRACTION FROM THE STREAM USING COMMA SEPERATION
             if(getline(data_stream,raw_id,',')&& getline(data_stream,raw_name,',')&& getline(data_stream,raw_salary,',')&&getline(data_stream,raw_dept,',')){
                 // PART 5 NEW: EXCEPTION HANDLING (try-catch) to guard against corrupted data TYPES
                 try{
-                    //PART 3 NEW: OBJECT INSTANTIATION AND STRING TO INTEGER DATA CASTING (stio)
+            //PART 3 NEW: OBJECT INSTANTIATION AND STRING TO INTEGER DATA CASTING (stio)
                 Employee temp_emp;
                 temp_emp.id = stoi(raw_id); // coverting string id to integer
                 temp_emp.name = raw_name;
                 temp_emp.salary = stoi(raw_salary); //coverting string salary to integer
                 temp_emp.department = raw_dept;
-                // PART 3 NEW: PUCH THE CONSTRUCTED OBJECT DIRECTLY INTO RAM DATABASE ARRAY
-                parsed_database[active_record_count] = temp_emp;
-                active_record_count++;
-                //logging current state telemetry
-                cout<<"[PARSED ROW #" <<row_counter<<"]: mapped"<<temp_emp.name<<"securely into ram memory matrix location index["<<active_record_count-1<<"]"<<endl;
+                //PART 7: UPGRADE : DYNAMICALLY PUSH VALIDATED OBJECT INTO VECTOR RAM MEMORY
+                parsed_database.push_back(temp_emp);
+                cout<<"[PARSED ROW #]"<<row_counter<<"]: MAPPED"<<temp_emp.name<<"securely into RAM memory index["<<parsed_database.size()-1<<"]"<<endl;
             } catch (const invalid_argument& e){
                 // PART 5: CATCHES INVALIED DATA FORMAT (eg, non-numertic string passed to stoi)
                 cout<<"[SKIPPED BAD ROW #"<<row_counter<<"]: invalid number format in row:'"<<current_row<<"'"<<endl;
@@ -111,7 +106,7 @@ class highfrequencyparser {
             }
          }
          cout<<"==========================================================================="<<endl;
-         cout<<"[SUCCESS]: parasing complete. total"<<active_record_count<<"objects generated in ram.\n"<<endl;
+         cout<<"[SUCCESS]: parasing complete. total"<<parsed_database.size()<<"objects generated in ram.\n"<<endl;
          file.close();
     }
         /**
@@ -122,7 +117,7 @@ class highfrequencyparser {
             cout<<"=============================================================="<<endl;
             cout<<"INDEX\tID\tNAME\t\tSALARY\t\tDEPARTMENT"<<endl;
             cout<<"============================================================"<<endl;
-            for(int i=0;i<active_record_count;i++){
+            for(int i=0;i<parsed_database.size();i++){
                 cout<<i<<"\t"
                     <<parsed_database[i].id<<"\t"
                     <<parsed_database[i].name<<"\t\t"
@@ -142,14 +137,14 @@ class highfrequencyparser {
             cout<<"\n[QUERY ENGINE:SEARCH BY ID ="<<target_id<<"]"<<endl;
             // flag to track wherther the target  ID was found during iteration
             bool found= false;
-            // iteration through all active records in the database
-            for( int i= 0; i<active_record_count; i++){
+            
+            for( const auto& emp : parsed_database){
                 //  check if the current record's id matches the target 
-                if( parsed_database[i].id==target_id){
-                    cout<<"[MATCH FOUND]: ID:"<<parsed_database[i].id
-                        <<"|NAME: "<<parsed_database[i].name
-                         <<"|SALARY: "<<parsed_database[i].salary
-                         <<"|DEPT: "<<parsed_database[i].department<<endl;
+                if( emp.id==target_id){
+                    cout<<"[MATCH FOUND]: ID:"<<emp.id
+                        <<"|NAME: "<<emp.name
+                         <<"|SALARY: "<<emp.salary
+                         <<"|DEPT: "<<emp.department<<endl;
                        found=true;
                         break;//stop the loop execution immediately once a match is confirmed
                 }
@@ -164,14 +159,14 @@ class highfrequencyparser {
           void find_highest_salary(){
               cout<<"\n [ANALYTICS ENGINE: FIND HIGHEST SALARY]"<<endl;
               // guard clause: enshure the database is not empty to prevent errors
-              if( active_record_count==0){
+              if( parsed_database.empty()){
                   cout<<"[ERROR]: DATABASE IS EMPTY"<<endl; 
                   return;
               }
               // initialize the first record as a baseline maximum
-              int max_index=0;
+              size_t max_index=0;
               // iterate starting from second record (index 1) to compare salaries
-              for( int i= 1; i<active_record_count; i++){
+              for( int i= 1; i<parsed_database.size(); i++){
                   //otherwise max_index if the larger salary value is encountered
                   if(parsed_database[i].salary> parsed_database[max_index].salary){
                       max_index = i;
@@ -191,14 +186,14 @@ class highfrequencyparser {
                 // counter variable to tally the number of matching records found 
                 int match_count = 0;
                 // iterate through the entire dataset to find the all the string matches
-                for( int i=0; i<active_record_count; i++){
+                for( const auto& emp : parsed_database){
                     // checks the current record's department alligns with the target query
-                    if( parsed_database[i].department == target_dept){
+                    if( emp.department == target_dept){
                         match_count++;
                         // display the specific details of the matched record
-                        cout<<"["<<match_count<<"] ID:"<<parsed_database[i].id
-                              <<"| NAME:"<< parsed_database[i].name
-                             <<"| SALARY:" << parsed_database[i].salary<<endl;
+                        cout<<"["<<match_count<<"] ID:"<<emp.id
+                              <<"| NAME:"<< emp.name
+                             <<"| SALARY:" << emp.salary<<endl;
                     }
                 }
                 // output final evalution logic based on the match counter state 
@@ -210,14 +205,36 @@ class highfrequencyparser {
                     
             }
             /** 
-            *@brief PART 6: SYSTEM DIAGNOSTIC METRICS DISPLAY INTERNAL MEMORY STATE.
+            *@brief PART 6 & 7: SYSTEM DIAGNOSTIC METRICS DISPLAY INTERNAL DYNAMIC MEMORY STATE.
             */
             void show_system_diagnostics(){
                 cout<<"[STSTEM DIAGNOSTIC METRICS]"<<endl;
                 cout<<"======================================"<<endl;
-                cout<<"-> valied records loaded in RAM   :"<<active_record_count<<endl;
-                cout<<"-> buffer Max capacity limit  :"<< MAX_CAPACITY<<endl;
-                cout<<"-> RAM Buffer utilization    :"<<((float)active_record_count/MAX_CAPACITY)*100<<"%"<<endl;
+                cout<<"-> ACTIVE VALID RECORDS IN RAM: "<< parsed_database.size()<<endl;
+                cout<<"-> ALLOCATION ENGINE TYPE : DYNAMIC STL VECTOR"<<endl;
+                cout<<"-> RAM CAPACITY STATE : Auto-expanding memory"<<endl;
                 cout<<"======================================"<<endl;
+            }
+            /** 
+            * @brief  PART 7: FLUSHES CLEANED DYNAMIC RAM DATA BACK TO THE CSV DISK STORAGE.
+            */
+            bool save_to_file(const string& filename){
+                ofstream file(filename);
+                if(!file.is_open()){
+                    cout<<"[ERROR][PART 7]: UNABLE TO OPEN FILE FOR DISK PERSISTENCE SYNC"<<endl;
+                    return false;
+                }
+                // write clean CSV HEADER 
+                file<<"ID,Name,Salery, Department\n";
+                //flush clean vector state to disk file
+                for(const auto& emp: parsed_database){
+                    file<<emp.id<<",";
+                    file<<emp.name<<",";
+                    file<<emp.department<<"\n";
+                }
+                file.close();
+                cout<<"\n [DISK PERSISTENCE SYNC][PART 7]: FLUSHED"<<parsed_database.size()
+                <<"clean records back to "<<filename<<" disk storage"<<endl;
+                return true;
             }
     };
